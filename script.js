dd6f77dc3dc02521a7d92bf2441d8ba3bf70e64c
Mizu <71164002+MiyagawaMizu@users.noreply.github.com>
2023-05-30 18:46:18 +0700

    new

--- script.js
+++ script.js
@@ -63,17 +63,17 @@ function ctrlShiftKey(e, keyCode) {
 	return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
 }
 
-document.onkeydown = (e) => {
-	// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
-	if (
-		event.keyCode === 123 ||
-		ctrlShiftKey(e, "I") ||
-		ctrlShiftKey(e, "J") ||
-		ctrlShiftKey(e, "C") ||
-		(e.ctrlKey && e.keyCode === "U".charCodeAt(0))
-	)
-		return false;
-};
+// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+// document.onkeydown = (e) => {
+// 	if (
+// 		event.keyCode === 123 ||
+// 		ctrlShiftKey(e, "I") ||
+// 		ctrlShiftKey(e, "J") ||
+// 		ctrlShiftKey(e, "C") ||
+// 		(e.ctrlKey && e.keyCode === "U".charCodeAt(0))
+// 	)
+// 		return false;
+// };
 
 // Pause and play audio
 var myAudio = document.getElementById("my-audio");
